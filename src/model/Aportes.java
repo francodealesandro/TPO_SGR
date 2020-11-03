0b5eb0a90e27b0564f7be8dfb076db7b88deb0fa
tomibrander <63247687+tomibrander@users.noreply.github.com>
2020-11-03 20:24:56 -0300

    creacion de clases
    creacion de clases acciones, accionista, aportes, cambioEstado, documentacion

---
+++ src/model/Aportes.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.util.Date;
+
+public class Aportes {
+    private int monto;
+    private Date fecha;
+    private String estadoVigencia;
+}
---
+++ src/model/CambioEstado.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.util.Date;
+
+public class CambioEstado {
+    private String estadoAnterior;
+    private String estadoNuevo;
+    private Date fechaDeCambio;
+}
