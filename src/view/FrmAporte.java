b80681b52cfb09f1373ce50cd3c7556617b16692
tomibrander <63247687+tomibrander@users.noreply.github.com>
2020-11-19 20:18:33 -0300

    FrmAporte
    FrmAporte sin logica, solo vista

--- src/view/FrmAporte.java
+++ src/view/FrmAporte.java
@@ -5,4 +5,9 @@ import javax.swing.*;
 public class FrmAporte {
     private JPanel pnlPrincipal;
     private JPanel pnlTitulo;
+    private JComboBox comboSocio;
+    private JLabel lblMonto;
+    private JLabel lblSocio;
+    private JTextField txtMonto;
+    private JButton btnAceptar;
 }
